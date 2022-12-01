3efd19ece365401fd42271b9e3316de3ebd8b579
Oktay Berk Altıntaş <65086193+oktayberk@users.noreply.github.com>
2022-12-01 17:53:13 +0300

    Update 6.JavaLoopsI.java

--- 6.JavaLoopsI.java
+++ 6.JavaLoopsI.java
@@ -6,22 +6,15 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-
-       
+        
         for (int i = 1; i <11 ; i++) {
-
             System.out.println(N +" x "+ i +" = "+ N*i);
-
         }
 
-        
-        
     }
 }
